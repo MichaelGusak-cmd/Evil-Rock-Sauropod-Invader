ecb9397978d1c1f0c20b417114b1285a66a5f442
Mike Gusak <60900204+MichaelGusak-cmd@users.noreply.github.com>
2022-01-30 13:13:32 -0600

    Update SceneChanger.cs

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,12 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+	private string currScene;
+	public string prevScene;
+
+	void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);  //Allow this object to persist between scene changes
+		prevScene = "Menu";
+		currScene = "Menu";
+	}
+
 	public void ChangeScene(string sceneName)
 	{
-		SceneManager.LoadScene(name);
+		prevScene = currScene;
+		currScene = sceneName;
+		SceneManager.LoadScene(sceneName);
 	}
-	public void Exit()
+
+	public void PrevScene() 
 	{
-		Application.Quit();
+		currScene = prevScene;
+		prevScene = "DeathMenu";
+		SceneManager.LoadScene(currScene);
 	}
 }
