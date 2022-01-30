9225f8a0ecb0d138515c69727906ee91a813ffa8
millandavid <71908868+millandavid@users.noreply.github.com>
2022-01-30 17:52:59 -0600

    presentation version

---
+++ Assets/xbox.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xbox : MonoBehaviour
+{
+
+    public GameObject enemy;
+
+    public float range = 3F;
+    public float health = 10;
+    public float objectSpeed = 0.00005F;
+
+    private float enemyX, baseX;
+    public float speed = 0.00005F;
+
+    private Vector2 move = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start(){
+        enemyX = transform.position.x; //get current position
+        baseX = enemyX; // save origin position
+        speed = objectSpeed; // speed constant
+    }
+
+    // Update is called once per frame
+    void Update(){
+      if (enemyX > baseX+range){
+          move.x = 0;
+          speed = -objectSpeed;
+      }
+      
+      if (enemyX < baseX-range) {
+          move.x = 0; 
+          speed = objectSpeed; 
+      }
+
+      move.x += speed;
+      enemyX = transform.position.x;
+      
+      transform.Translate(move); // this is what moves the object
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+      if(col.CompareTag("PlayerWeapon")) {
+        health-=1;
+        if(health == 0){
+          Destroy(enemy.gameObject);
+        }
+        
+      }
+    }
+}
