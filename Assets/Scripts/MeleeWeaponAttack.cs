7ada815ad137f4a01138d7f345a173804d0f423b
danieldebassige <danieldebassige@gmail.com>
2022-01-30 02:27:01 -0600

    Fixed Weapon Animation and Player getting stuck on walls

--- Assets/Scripts/MeleeWeaponAttack.cs
+++ Assets/Scripts/MeleeWeaponAttack.cs
@@ -14,6 +14,10 @@ public class MeleeWeaponAttack : MonoBehaviour
     private Vector2 centre;
     private Vector2 pos;
 
+    private int adjustX = 4;
+    private int adjustY = 3;
+    private bool attacking = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +28,7 @@ public class MeleeWeaponAttack : MonoBehaviour
         pos = transform.position;
         pos = new Vector2(pos.x + size.x/2 * dir, pos.y - size.y/6);
         weapon = Instantiate(weapon, pos, Quaternion.identity);
-        weapon.transform.Rotate(new Vector3(0,0,0));
+        weapon.transform.Rotate(new Vector3(0,0,90));
     }
 
     // Update is called once per frame
@@ -37,11 +41,28 @@ public class MeleeWeaponAttack : MonoBehaviour
         }
         
         pos = transform.position;
-        pos = new Vector2(pos.x + size.x/2 * dir, pos.y - size.y/10);
+        pos = new Vector2(pos.x + size.x/adjustX * dir, pos.y + size.y/adjustY);
         weapon.transform.position = pos;
-        weapon.transform.Rotate(new Vector3(0,0,5));
+        //weapon.transform.Rotate(new Vector3(0,0,5));
+        if(Input.GetKeyDown("space") && !attacking)
+        StartCoroutine(SwingWeapon());
 
         
+    }
+
+    IEnumerator SwingWeapon() 
+    {
+        attacking = true;
+        weapon.transform.Rotate(new Vector3(0,0,-90 * dir));
+        adjustX = 1;
+        adjustY = -10;
+        yield return new WaitForSeconds( 0.3f );
+        adjustX = 4;
+        adjustY = 3;
+        weapon.transform.Rotate(new Vector3(0,0,90 * dir));
+        yield return new WaitForSeconds( 0.2f );
+        attacking = false;
+
     }
 }
 
